0bbfcb913a402f9521a6cc181db03f68d2cca9af
Ole Erik Brennhagen <github.eloelo@spamgourmet.com>
2019-07-17 15:13:02 +0200

    Add ffmenu

---
+++ js/ffmenu.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+if (document.images) {
+    img1 = new Image();
+    img1.src = "./img/ffbuttoninactive.png";
+    img2 = new Image();
+    img2.src = "./img/ffbuttonactive.png";
+}
+$('#ffmenu').load('./ffmenu.html');
+});
+
+
