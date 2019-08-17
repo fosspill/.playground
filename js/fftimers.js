a613643996270ff44346acd4894102f4df47100d
fosspill <github.eloelo@spamgourmet.com>
2019-08-17 20:19:32 +0200

    Update fftimers.js

--- js/fftimers.js
+++ js/fftimers.js
@@ -9,24 +9,30 @@ var resultDate = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDat
     return resultDate;
 }
 
-function timerTo(elementid, cddate) {
-var countDownDate = cddate.getTime();
+function timerTo(elementid, cddate, title) {
+    var countDownDate = cddate.getTime();
+    var divtimer = document.createElement("div");
+    var spantimer = document.createElement("div");
+    divtimer.innerHTML = title + "<br>";
+    spantimer.setAttribute("id", elementid);
+    divtimer.appendChild(spantimer);
+    document.getElementById("timerslocal").appendChild(divtimer);
+    
+    setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.getElementById(elementid).innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
 
-  document.getElementById(elementid).innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  
-  if (distance < 0) {
-     document.getElementById(elementid).innerHTML = "Ongoing or Ended";
-  }
-}, 1000);
+      if (distance < 0) {
+         document.getElementById(elementid).innerHTML = "Ongoing or Ended";
+      }
+    }, 1000);
 }
 
 $(document).ready(function() {
